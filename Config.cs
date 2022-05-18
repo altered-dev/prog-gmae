ba24d7222b00bdd9efdbda295e964b587d0eb62c
altered <altered_games@outlook.com>
2022-05-18 17:34:02 +0500

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,10 @@
+using System.Numerics;
+
+public static class Config
+{
+	public const int WindowWidth = 1280;
+	public const int WindowHeight = 720;
+	public const string WindowName = "haha yes";
+
+	public static readonly Vector2 WindowCenter = new(WindowWidth / 2, WindowHeight / 2);
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+#region initialization
+
+InitWindow(Config.WindowWidth, Config.WindowHeight, Config.WindowName);
+var camera = new Camera2D(Vector2.Zero, -Config.WindowCenter, 0f, 1f);
+
+#endregion
+
+#region main game loop
+
+while (!WindowShouldClose())
+{
+	BeginDrawing();
+	BeginMode2D(camera);
+	ClearBackground(Color.WHITE);
+	DrawPixel(0, 0, Color.RED);
+	EndMode2D();
+	EndDrawing();
+}
+
+#endregion
+
+CloseWindow();
